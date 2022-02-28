287e01995b1a83d7a506fff4d3467906218cab5a
Alexandr Martirosyan <alexandr.martirosyan2000@gmaiL.com>
2022-02-28 20:02:12 +0400

    some improvements

--- contracts/StakingToken.sol
+++ contracts/StakingToken.sol
@@ -1,17 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import { ERC1155Receiver } from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 
-contract StakingToken is Ownable {
-
-  event Received(address sender, uint256 amount);
-  event Staked(address indexed stakeholder, uint256 tokenId, uint256 amount, uint256 timestamp);
+contract StakingToken is ERC1155Receiver, Ownable {
+  event Staking(
+    address indexed stakeholder,
+    uint256 indexed tokenId,
+    uint256 amount,
+    uint256 timestamp,
+    bool indexed stake
+  );
 
   /**
-   * A stake struct is used to represent the way we store stakes, 
+   * A stake struct is used to represent the way we store stakes,
    */
   struct Stake {
     uint256 tokenId;
@@ -31,14 +35,17 @@ contract StakingToken is Ownable {
    * @param tokenId The token which will be stacked.
    * @param amount The amount of token.
    */
-  function stake(IERC1155 tokenAddress, uint256 tokenId, uint256 amount) public
-  {
+  function stake(
+    IERC1155 tokenAddress,
+    uint256 tokenId,
+    uint256 amount
+  ) public {
     require(stakes[msg.sender].tokenId == 0, "You already have a staked token");
 
-    stakes[msg.sender] = Stake(tokenId, amount, block.timestamp);
+    stakes[msg.sender] = Stake({ tokenId: tokenId, amount: amount, timestamp: block.timestamp });
     IERC1155(tokenAddress).safeTransferFrom(msg.sender, address(this), tokenId, amount, "0x00");
 
-    emit Staked(msg.sender, tokenId, amount, block.timestamp);
+    emit Staking(msg.sender, tokenId, amount, block.timestamp, true);
   }
 
   /**
@@ -46,8 +53,7 @@ contract StakingToken is Ownable {
    *
    * @param tokenAddress The token contract address.
    */
-  function unstake(IERC1155 tokenAddress) public
-  {
+  function unstake(IERC1155 tokenAddress) public {
     IERC1155(tokenAddress).safeTransferFrom(
       address(this),
       msg.sender,
@@ -56,37 +62,33 @@ contract StakingToken is Ownable {
       "0x00"
     );
 
+    emit Staking(
+      msg.sender,
+      stakes[msg.sender].tokenId,
+      stakes[msg.sender].amount,
+      block.timestamp,
+      false
+    );
     delete stakes[msg.sender];
   }
 
-  /**
-   * Handles the receipt of a single ERC1155 token type. This function is
-   * called at the end of a `safeTransferFrom` after the balance has been updated.
-   *
-   * NOTE: To accept the transfer, this must return
-   * `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))`
-   * (i.e. 0xf23a6e61, or its own function selector).
-   *
-   * @param operator The address which initiated the transfer (i.e. msg.sender)
-   * @param from The address which previously owned the token
-   * @param id The ID of the token being transferred
-   * @param value The amount of tokens being transferred
-   * @param data Additional data with no specified format
-   * @return `bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"))` if transfer is allowed
-   */
   function onERC1155Received(
-    address operator,
-    address from,
-    uint256 id,
-    uint256 value,
-    bytes calldata data
-  ) external returns (bytes4)
-  {
+    address,
+    address,
+    uint256,
+    uint256,
+    bytes calldata
+  ) external pure override returns (bytes4) {
     return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
   }
 
-  receive() external payable
-  {
-    emit Received(msg.sender, msg.value);
+  function onERC1155BatchReceived(
+    address,
+    address,
+    uint256[] calldata,
+    uint256[] calldata,
+    bytes calldata
+  ) external pure override returns (bytes4) {
+    return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
   }
 }
